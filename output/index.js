c77b7c61650d8a788055dc32acb6f9b013ca08ce
Md. Ahosan Habib <cseahosan@gmail.com>
2022-06-05 11:53:14 +0600

    use in class

--- output/index.js
+++ output/index.js
@@ -1,2 +1,14 @@
 "use strict";
-let myFunc; //giving function signature. syntax like arrow function but not arrow function
+class Player {
+    constructor(n, a, c) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+    play() {
+        console.log(`${this.name} from ${this.country} is playing.`);
+    }
+}
+const mashrafi = new Player('Mashrafi', 40, 'Bangladesh');
+const players = []; // players array must be an object of Player ckass
+players.push(mashrafi); // example
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-let myFunc: (x: string, y:string) => void;//giving function signature. syntax like arrow function but not arrow function
+class Player {
+    name: string;
+    age: number;
+    country: string;
+
+    constructor(n: string, a: number, c: string){
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+
+    play() {
+        console.log(`${this.name} from ${this.country} is playing.`);
+    }
+}
+
+const mashrafi = new Player('Mashrafi', 40, 'Bangladesh');
+
+const players: Player[] = []; // players array must be an object of Player ckass
+
+players.push(mashrafi); // example
